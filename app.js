f61a2d8753509e521e947ec78a4a05f1f544fe8b
WilliamTsou818 <cccc1589@gmail.com>
2021-05-18 15:44:40 +0800

    feat : add mongoose and models directory

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true,   useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoDB error!')
+})
+
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello')
