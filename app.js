47e92aeba7f09912c46b5f96f9919c754a8a5999
WilliamTsou818 <cccc1589@gmail.com>
2021-05-26 16:18:51 +0800

    refactor : modify mongoose config

--- app.js
+++ app.js
@@ -4,25 +4,13 @@ const app = express()
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 
-// setting database and mongoose
-const mongoose = require('mongoose')
-const Todo = require('./models/todo')
-
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true,   useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongoDB error!')
-})
-
-db.once('open', () => {
-  console.log('mongoDB connected!')
-})
+// include mongoose
+require('./config/mongoose')
 
 // include routes
 const routes = require('./routes')
 
+// set visual template
 app.engine('hbs', exphbs({ defaultLayout : 'main', extname : '.hbs' }))
 app.set('view engine', 'hbs')
 
---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+// setting database and mongoose
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongoDB error!')
+})
+
+db.once('open', () => {
+  console.log('mongoDB connected!')
+})
+
+module.exports = db
