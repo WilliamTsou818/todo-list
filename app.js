05237aabb58cc09c55335b9fc5d09c60d4fcb5f9
WilliamTsou818 <cccc1589@gmail.com>
2021-05-26 15:35:27 +0800

    refactor : modify express routes

--- app.js
+++ app.js
@@ -20,71 +20,16 @@ db.once('open', () => {
   console.log('mongoDB connected!')
 })
 
+// include routes
+const routes = require('./routes')
+
 app.engine('hbs', exphbs({ defaultLayout : 'main', extname : '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended : true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  Todo.find()
-    .lean()
-    .sort({ _id : 'asc' }) // asc : 升冪 || desc : 降冪
-    .then(todos => res.render('index', { todos }))
-    .catch(error => console.error(error))
-})
-
-app.get('/todos/new', (req, res) => {
-  res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name
-  // const todo = new Todo({ name })
-  // return Todo.save()
-  //   .then(() => res.redirect('/'))
-  //   .catch(error => console.log(error))
-  return Todo.create({ name })
-    .then(() => res.redirect('/'))
-    .catch(error => console.error(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('detail', { todo }))
-    .catch(error => console.error(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then(todo => res.render('edit', { todo }))
-    .catch(error => console.error(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 app.listen(3000, ()=> {
   console.log('Server is running on http://localhost:3000')
